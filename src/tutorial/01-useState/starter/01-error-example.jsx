6146e0d258fab742a22b068c59b182e9e80270e1
Eric Lee <ericleecodes@gmail.com>
2025-01-10 23:20:05 -0800

    Set up error example

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,19 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const count = 0;
+  const increaseNumber = () => {
+    count = count + 1;
+  };
+
+  const decreaseNumber = () => {
+    count = count - 1;
+  };
+  return (
+    <div className="container">
+      <div>{count}</div>
+      <button onClick={increaseNumber}>increase</button>
+      <button>decrease</button>
+    </div>
+  );
 };
 
 export default ErrorExample;
