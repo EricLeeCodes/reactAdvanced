f1a539eefee095a4f8b57341bea16fd46000962f
Eric Lee <ericleecodes@gmail.com>
2025-01-12 23:27:01 -0800

    Add a decrease button

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -2,14 +2,21 @@ import { useState } from "react";
 
 const UseStateBasics = () => {
   const [count, setCount] = useState(0);
-  const handleClick = () => {
+  const increaseCount = () => {
     setCount(count + 1);
   };
-
+  const decreaseCount = () => {
+    setCount(count - 1);
+  };
   return (
     <>
       <h4>You clicked {count} times</h4>
-      <button type="button" className="btn" onClick={handleClick}></button>
+      <button type="button" className="btn" onClick={decreaseCount}>
+        -
+      </button>
+      <button type="button" className="btn" onClick={increaseCount}>
+        +
+      </button>
     </>
   );
 };
