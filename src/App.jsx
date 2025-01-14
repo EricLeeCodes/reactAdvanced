b7de1538d790dabe57af63cae7ad5badc1f7687c
Eric Lee <ericleecodes@gmail.com>
2025-01-13 23:12:07 -0800

    Set up useState for displaying array data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Starter from "./tutorial/01-useState/starter/02-useState-basics.jsx";
+import Starter from "./tutorial/01-useState/starter/03-useState-array.jsx";
+import Final from "./tutorial/01-useState/final/03-useState-array.jsx";
 
 function App() {
   return (
     <div className="container">
       <h2>Advanced React</h2>
       <Starter />
+      {/* <Final /> */}
     </div>
   );
 }
